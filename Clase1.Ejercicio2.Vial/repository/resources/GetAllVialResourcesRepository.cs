186f65fad65e5b73ba10a13a32b1c289e47570d8
davidJavac <david.abramovich84@gmail.com>
2021-03-28 16:34:55 -0300

    cleaned code

--- Clase1.Ejercicio2.Vial/repository/resources/GetAllVialResourcesRepository.cs
+++ Clase1.Ejercicio2.Vial/repository/resources/GetAllVialResourcesRepository.cs
@@ -2,7 +2,6 @@
 using Clase1Ejercicio2Vial.core.entity;
 using Clase1Ejercicio2Vial.repository.exception;
 using System;
-using Clase1Ejercicio2Vial.core.entity;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -20,17 +19,17 @@ namespace Clase1.Ejercicio2.Vial.repository
             {
                 Parallel.ForEach(File.ReadLines(path), (line, _, lineNumber) =>
                 {
-                        String[] fields = line.Split(",");
+                    String[] fields = line.Split(",");
 
-                        int n_sheet = Int32.Parse(fields[0]);
-                        String objectType = fields[1];
-                        int idSection = Int32.Parse(fields[2]);
-                        String pathType = fields[3];
-                        int lenght = Int32.Parse(fields[4]);
+                    int n_sheet = Int32.Parse(fields[0]);
+                    String objectType = fields[1];
+                    int idSection = Int32.Parse(fields[2]);
+                    String pathType = fields[3];
+                    int lenght = Int32.Parse(fields[4]);
 
                     VialDto vialDto = new VialDto(n_sheet, objectType, idSection, pathType, lenght);
 
-                        threadSafeCollection.Enqueue(vialDto);
+                    threadSafeCollection.Enqueue(vialDto);
                                
                 });
             }
