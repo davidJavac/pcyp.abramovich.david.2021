d7ade98e686d6230b20c40145701b85626a69c2d
davidJavac <david.abramovich84@gmail.com>
2021-04-02 15:12:47 -0300

    implemented word counting

---
+++ Clase3.Ejercicio3.WordsCount/interface-adapter/view/implementation/ConsoleWordCount.cs
@@ -0,0 +1,26 @@
+﻿using Clase3.Ejercicio3.WordCount.interface_adapter.view;
+using Clase3.Ejercicio3.WordCount.usecase.dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio3.WordsCount.interface_adapter.view.implementation
+{
+    class ConsoleWordCount : CollectionPresenter
+    {
+        public void present(Output output)
+        {
+            Dictionary<String, int> response = (Dictionary<String, int>)output.getResponse();
+
+            Console.WriteLine("Response message: " + output.Message);
+
+            foreach (KeyValuePair<String, int> entry in response) {
+
+                Console.WriteLine(entry.Key + " " + entry.Value);
+            }
+            Console.WriteLine("size: " + response.Count);
+        }
+    }
+}
---
+++ Clase3.Ejercicio3.WordsCount/usecase/implementation/WordCountProcess.cs
@@ -0,0 +1,46 @@
+﻿using Clase3.Ejercicio3.WordCount.usecase;
+using Clase3.Ejercicio3.WordCount.usecase.dto;
+using Clase3.Ejercicio3.WordsCount.usecase.dto.implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio3.WordsCount.usecase.implementation
+{
+    class WordCountProcess : CollectionProcess
+    {
+        public Output execute(Input input)
+        {
+            List<String> words = (List<String>)input.getValue();
+
+            Dictionary<String, int> wordsOcurrences = convertListToDictionary(words);
+
+            OutputDto output =  new OutputDto(wordsOcurrences);
+            output.Message = "successful response";
+
+            return output;
+        }
+
+        private Dictionary<String, int> convertListToDictionary(List<String> words) {
+
+            Dictionary<String, int> wordsOcurrences = new Dictionary<String, int>();
+            
+            words.ForEach(word =>
+            {
+                if (!wordsOcurrences.ContainsKey(word))
+                {
+                    wordsOcurrences.Add(word, 1);
+                }
+                else {
+                    int cont;
+                    wordsOcurrences.TryGetValue(word, out cont);
+                    wordsOcurrences[word] = cont + 1;
+                }
+            });
+
+            return wordsOcurrences;
+        }
+    }
+}
