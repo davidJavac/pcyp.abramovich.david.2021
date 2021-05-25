a149d4a36fa1525f0bd0552e7c58c95dc13d5fe5
davidJavac <david.abramovich84@gmail.com>
2021-05-24 21:38:13 -0300

    added dataflow pattern

---
+++ Clase10.Ejercicio1.Dataflow/Commander/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Commander
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.promedio = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.txtEstado.Enabled = false;
+            this.txtEstado.Location = new System.Drawing.Point(30, 51);
+            this.txtEstado.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.ReadOnly = true;
+            this.txtEstado.Size = new System.Drawing.Size(184, 20);
+            this.txtEstado.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Estado";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(82, 74);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(65, 25);
+            this.btnIniciar.TabIndex = 5;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // promedio
+            // 
+            this.promedio.AutoSize = true;
+            this.promedio.Location = new System.Drawing.Point(253, 54);
+            this.promedio.Name = "promedio";
+            this.promedio.Size = new System.Drawing.Size(65, 13);
+            this.promedio.TabIndex = 6;
+            this.promedio.Text = "Promedio C°";
+            this.promedio.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(342, 54);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 13);
+            this.resultado.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Promedio de C° de 3 componentes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 114);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.promedio);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEstado);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label promedio;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
